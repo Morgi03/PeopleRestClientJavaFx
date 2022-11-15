654e79b131a04e024b3a56deb9c8a8238273f0fa
Morgován Nimród <morgovannimrod22@gmail.com>
2022-11-15 15:56:14 +0100

    runlater és táblázat megírása

--- src/main/java/hu/petrik/peoplerestclientjavafx/App.java
+++ src/main/java/hu/petrik/peoplerestclientjavafx/App.java
@@ -8,11 +8,13 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class App extends Application {
+    public static final String BASE_URL = "https://retoolapi.dev/0xaaMh/people";
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("list-people-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
-        stage.setTitle("People ");
+        stage.setTitle("People");
         stage.setScene(scene);
         stage.show();
     }
